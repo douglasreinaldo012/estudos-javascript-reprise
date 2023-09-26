e8194fdb83d51d46dc93144eacc366485b1e34d3
Douglas Reinaldo <123218252+douglasreinaldo012@users.noreply.github.com>
2023-09-25 23:31:53 -0300

    up

---
+++ 4-modulo-D/condicoes.js
@@ -0,0 +1,8 @@
+var vel = 61;
+console.log(`Sua velocidade foi ${vel}km/h `)
+if(vel>60){
+    console.log('você ultrapassou o limite de velocidade,Multado!')
+}else{
+    console.log('Um motorista conciente, salva vidas!')
+}
+console.log('dirija com segurança, use o cinto!')
