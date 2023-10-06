c2fb141e99d1c150c93a365cb52865ac9e4e3aa9
Douglas Reinaldo <123218252+douglasreinaldo012@users.noreply.github.com>
2023-10-05 21:29:01 -0300

    UP

---
+++ 5-modulo-E/aula1.js
@@ -0,0 +1,15 @@
+//estrututa de repetição com teste logico no inicio
+console.log("-------TESTE WHILE--------------------")
+var c = 1;
+while(c<=6){
+    console.log(`passo ${c}`);
+    c++;
+}
+
+//estrutura de repetição com teste lógico no fim
+console.log("--------TESTE DO WHILE----------------")
+c=1;
+do {
+    console.log(`passo ${c}`);
+    c++;
+}while(c<=6)
--- exercicios/verifica_idade/main.js
+++ exercicios/verifica_idade/main.js
@@ -8,7 +8,7 @@ var imagem = document.getElementById('imagem')
 
 if(ano.value.length == 0 || ano.value > ano_atual){
     window.alert('ERRO, Verrifique se os dados estão preechidos corretamente!')
-}else {
+}else{
         var idade = ano_atual - Number(ano.value);
         var fsex = document.getElementsByName ('radsex');
         var genero = ''
@@ -25,59 +25,59 @@ if(ano.value.length == 0 || ano.value > ano_atual){
                 texto.innerText = `Você tem ${idade} anos de idade e é ${genero}`
                 imagem.src = './img/bebe-homem.png'
 
-                }else if(fsex[1].checked && idade>=0 && idade <= 1){
+        }else if(fsex[1].checked && idade>=0 && idade <= 1){
 
-                        genero = "uma menina"
-                        texto.innerText = `Você tem ${idade} ano de idade e é ${genero}`
-                        imagem.src = './img/bebe-menina.png'
+                genero = "uma menina"
+                texto.innerText = `Você tem ${idade} ano de idade e é ${genero}`
+                imagem.src = './img/bebe-menina.png'
     
-                }else if(fsex[1].checked && idade>0 && idade <= 4){
+        }else if(fsex[1].checked && idade>0 && idade <= 4){
 
-                        genero = "uma menina"
-                        texto.innerText = `Você tem ${idade} anos de idade e é ${genero}`
-                        imagem.src = './img/bebe-menina.png'
+                genero = "uma menina"
+                texto.innerText = `Você tem ${idade} anos de idade e é ${genero}`
+                imagem.src = './img/bebe-menina.png'
 
-                        }else if(fsex[0].checked && idade>4 && idade <= 12){
+        }else if(fsex[0].checked && idade>4 && idade <= 12){
 
-                                genero = "um rapaz"
-                                texto.innerText = `Você tem ${idade} anos de idade e é ${genero}`
-                                imagem.src = './img/menino.png'
+                genero = "um rapaz"
+                texto.innerText = `Você tem ${idade} anos de idade e é ${genero}`
+                imagem.src = './img/menino.png'
 
-                                }else if(fsex[1].checked && idade>4 && idade <= 12){
+        }else if(fsex[1].checked && idade>4 && idade <= 12){
 
-                                    genero = "uma Moça"
-                                    texto.innerText = `Você tem ${idade} anos de idade e é ${genero}`
-                                    imagem.src = './img/menina.png'
+                genero = "uma Moça"
+                texto.innerText = `Você tem ${idade} anos de idade e é ${genero}`
+                imagem.src = './img/menina.png'
     
-                                    }else if(fsex[0].checked && idade>12 && idade <= 59){
+        }else if(fsex[0].checked && idade>12 && idade <= 59){
 
-                                        genero = "um Homem"
-                                        texto.innerText = `Você tem ${idade} anos de idade e é ${genero}`
-                                        imagem.src = './img/homem.png'
+                genero = "um Homem"
+                texto.innerText = `Você tem ${idade} anos de idade e é ${genero}`
+                imagem.src = './img/homem.png'
         
-                                        }else if(fsex[1].checked && idade>12 && idade <= 59){
+        }else if(fsex[1].checked && idade>12 && idade <= 59){
 
-                                            genero = "uma Mulher"
-                                            texto.innerText = `Você tem ${idade} anos de idade e é ${genero}`
-                                            imagem.src = './img/mulher.png'
+                genero = "uma Mulher"
+                texto.innerText = `Você tem ${idade} anos de idade e é ${genero}`
+                imagem.src = './img/mulher.png'
             
-                                            }else if(fsex[0].checked && idade>59){
+        }else if(fsex[0].checked && idade>59){
 
-                                                genero = "uma idoso"
-                                                texto.innerText = `Você tem ${idade} anos de idade e é ${genero}`
-                                                imagem.src = './img/idoso.png'
+                genero = "uma idoso"
+                texto.innerText = `Você tem ${idade} anos de idade e é ${genero}`
+                imagem.src = './img/idoso.png'
                 
-                                                }else if(fsex[1].checked && idade>59){
+        }else if(fsex[1].checked && idade>59){
 
-                                                    genero = "uma idosa"
-                                                    texto.innerText = `Você tem ${idade} anos de idade e é ${genero}`
-                                                    imagem.src = './img/idosa.png'
+                genero = "uma idosa"
+                texto.innerText = `Você tem ${idade} anos de idade e é ${genero}`
+                imagem.src = './img/idosa.png'
                     
-                                                    }else{
-                                                            genero = ""
-                                                            texto.innerText = `ERRO!`
-                                                            imagem.src = ' '
-                                                     }
+        }else{
+                genero = ""
+                texto.innerText = `ERRO!`
+                imagem.src = ' '
+        }
     
 }   
 }
